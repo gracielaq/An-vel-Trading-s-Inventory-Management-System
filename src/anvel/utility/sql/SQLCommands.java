428a89862fe87c06118189a8804a9a3d3127864c
jude89654 <jude89654@gmail.com>
2016-02-22 02:21:54 +0800

    minor fixes

--- src/anvel/utility/sql/SQLCommands.java
+++ src/anvel/utility/sql/SQLCommands.java
@@ -54,19 +54,7 @@ public interface SQLCommands {
 	String SEARCH_FOR_DELIVERY_REPORT = "select * from DeliveryDB where" + "DeliveryNum like ? OR "
 			+ "Driver like ? OR " + "Helper like ? OR " + "PlateNum like ? OR " + "CodingDay like ? OR "
 			+ "DeliveryDate like ? ";
-<<<<<<< HEAD
 
-	/*
-	 * FOR Products: FOR Products: CREATE TABLE `Product` ( `product_code`
-	 * int(11) NOT NULL, `supplier` varchar(200) NOT NULL, `delivery_date`
-	 * datetime NOT NULL, `date_received` datetime NOT NULL, `delivery_charge`
-	 * varchar(45) NOT NULL, `DR_SI` varchar(100) NOT NULL, `quantity` int(11)
-	 * NOT NULL, `product_description` varchar(999) DEFAULT NULL, `unit_price`
-	 * double NOT NULL, `discount_add` double DEFAULT NULL, `total_amount`
-	 * double DEFAULT NULL, `mode_of_payment` varchar(45) NOT NULL, PRIMARY KEY
-	 * (`product_code`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-	 */
-=======
 	
 	/*FOR Products:
 	CREATE TABLE `Product` (
@@ -86,7 +74,7 @@ public interface SQLCommands {
   PRIMARY KEY (`product_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 */
->>>>>>> origin/master
+
 
 	String UPDATE_PRODUCT = "update product set "
 			+ "supplier=?," + "delivery_date=?," + "date_recieved=?,"
@@ -95,16 +83,6 @@ public interface SQLCommands {
 			+ "total_amount=?," +"mode_of_payment=?,check_no=? "+ "where product_code=?";
 	String GET_ALL_PRODUCTS = "select * from product";
 	String SEARCH_PRODUCT = "select * from product where product_code=?";
-<<<<<<< HEAD
-	String UPDATE_PRODUCT = "update product set delivery_date=?," + "date_received=?," + "delivery_charge=?,"
-			+ "DR_SI=?," + "quantity=?," + "product_description=?," + "unit_price=?," + "discount_add=?,"
-			+ "total_amount=?," + "mode_of_payment=?," + "supplier=?" + "where product_code=?";
-	String ADD_PRODUCT = "insert into Product(delivery_date, date_received, delivery_charge, DR_SI, quantity,product_description,unit_price,discount_add,total_amount,mode_of_payment,supplier,product_code,check_no) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	String SEARCH_FOR_PRODUCTS = "select * from product where delivery_date like ? OR " + "date_received like ? OR "
-			+ "delivery_charge like ? OR " + "DR_SI like ? OR " + "quantity like ? OR "
-			+ "product_description like ? OR " + "unit_price like ? OR " + "discount_add like ? OR "
-			+ "total_amount like ? OR " + "mode_of_payment like ? OR " + "supplier like ? OR" + " product_code like ?";
-=======
 
 	String ADD_PRODUCT = "insert into Product(" + "delivery_date,"
 			+ "date_recieved, " + "delivery_charge," + "DR_SI, " + "quantity,"
@@ -124,7 +102,7 @@ public interface SQLCommands {
 			+ "supplier like ? OR"
 			+ " product_code like ?";
 
->>>>>>> origin/master
+
 
 	String ADD_TRUCK = "insert into product(plate_no,coding,model) values (?,?,?)";
 }
