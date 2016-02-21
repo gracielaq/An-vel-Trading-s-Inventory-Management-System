dc7715f4fa3eb93f79524f80be6f759f15cd2c57
gracielaq <gqstar30@gmail.com>
2016-02-22 04:01:57 +0800

    yaa deleteeeeee yazzz

--- src/anvel/controller/DeleteProductServlet.java
+++ src/anvel/controller/DeleteProductServlet.java
@@ -1,6 +1,7 @@
 package anvel.controller;
 import anvel.utility.sql.SQLOperations;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 /**
  * Created by Jude on 2/22/2016.
@@ -25,11 +27,26 @@ public class DeleteProductServlet extends HttpServlet {
         }
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getParameter("action").equals("yes")){
-            //TODO
+    try{
+    	RequestDispatcher dispatcher = null; 
+    	if(request.getParameter("action").equals("yes")){
+           int product_code=Integer.parseInt((String)request.getAttribute("productBean"));
+           
+           SQLOperations.transferProduct(product_code, connection);
+			SQLOperations.deleteProduct(product_code, connection);
+			ResultSet rs = 
+					  SQLOperations.getAllProducts(connection);
+			request.setAttribute("recordAdmins", rs);
+			dispatcher = 
+			 getServletContext().getRequestDispatcher("/View.html");	
+           
         }else{
             response.sendRedirect("/View.html");
         }
+    }catch (Exception e) {
+		System.err.println("Exception e - " + e.getMessage());
+		e.printStackTrace();
+	} 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/ViewProduct.jsp
+++ web/ViewProduct.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <jsp:useBean id="productrecords" type="java.sql.ResultSet" scope="request"/>
+<jsp:useBean id="productrecords" type="java.sql.ResultSet" scope="request"/>
     
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
